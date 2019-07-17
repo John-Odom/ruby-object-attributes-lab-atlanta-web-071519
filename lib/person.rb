6df467082c5471c143f1df2eb7a8de24bc587b77
John Odom <joodom91@gmail.com>
2019-07-17 01:45:39 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,14 @@
-class Dog
+class Person
   def name 
     @name
   end
   def name=(new_name)
     @name = new_name
   end
-  def breed
-    @breed
+  def job
+    @job
   end
-  def breed=(new_breed)
-    @breed=new_breed
+  def job=(new_job)
+    @job=new_job
   end
 end
